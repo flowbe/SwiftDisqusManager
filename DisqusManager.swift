4e733eb626f0e81606bdfdac9d9b835ccd504de4
Flow <theflow@outlook.com>
2015-03-09 19:12:34 +0100

    Add new func

--- DisqusManager.swift
+++ DisqusManager.swift
@@ -13,7 +13,57 @@ class DisqusManager {
     let secretAPIKey = "" // Your Disqus secret API key
     let forum = "" // Your forum shortname
     
-    func listPostsInThread(threadLink: String, completionHandler: (comments: [Comment]) -> ()) {
+    func listPostsInThread(threadID: String, completionHandler: (comments: [Comment]) -> ()) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true // Afficher l'icône de chargement dans la barre de status
+        var url = NSURL(string: "http://disqus.com/api/3.0/threads/listPosts.json?api_key=\(publicAPIKey)&thread=\(threadID)")! // URL du JSON
+        var request = NSURLRequest(URL: url) // Création de la requête HTTP
+        var queue = NSOperationQueue()  // Création de NSOperationQueue à laquelle le bloc du gestionnaire est distribué lorsque la demande complète ou échoué
+        
+        var comments = [Comment]()
+        
+        // Envoi de la requête asynchrone en utilisant NSURLConnection
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler:{(response: NSURLResponse!, data: NSData!, error: NSError!) ->Void in
+            // Gestion des erreurs de connexion
+            if error != nil {
+                // Masquer l'icône de chargement dans la barre de status
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+                
+                // Afficher le message d'erreur dans la console et avec une alertView
+                println(error.localizedDescription)
+                let errorAlert = UIAlertView(title: "Erreur", message: error.localizedDescription, delegate: self, cancelButtonTitle: "OK")
+                errorAlert.show()
+            }
+            else {
+                // Récupération du JSON
+                let json = JSON(data: data)
+                
+                for comment in json["response"].arrayValue {
+                    var dateFormat = NSDateFormatter()
+                    dateFormat.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+                    
+                    let id = comment["id"].intValue
+                    let parent = comment["parent"].int
+                    let likes = comment["likes"].intValue
+                    let dislikes = comment["dislikes"].intValue
+                    let isApproved = comment["isApproved"].boolValue
+                    let author = comment["author"]["name"].stringValue
+                    let message = comment["raw_message"].stringValue
+                    let date = dateFormat.dateFromString(comment["createdAt"].stringValue)!
+                    
+                    comments.append(Comment(id: id, parent: parent, likes: likes, dislikes: dislikes, isApproved: isApproved, author: author, message: message, date: date))
+                }
+            }
+            
+            completionHandler(comments: comments)
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                // Masquer l'icône de chargement dans la barre de status
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+            })
+        })
+    }
+    
+    func listPostsInThread(link threadLink: String, completionHandler: (comments: [Comment]) -> ()) {
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true // Afficher l'icône de chargement dans la barre de status
         var url = NSURL(string: "http://disqus.com/api/3.0/threads/listPosts.json?api_key=\(publicAPIKey)&forum=\(forum)&thread:link=\(threadLink)")! // URL du JSON
         var request = NSURLRequest(URL: url) // Création de la requête HTTP
