ca21a8c2209647e2782ca15f42dbdf6ec3279400
Flow <theflow@outlook.com>
2015-04-24 13:29:54 +0200

    Update DisqusManager.swift

--- DisqusManager.swift
+++ DisqusManager.swift
@@ -126,10 +126,9 @@ class DisqusManager {
     }
     
     func listPostsInThread(ident threadIdent: String, completionHandler: (comments: [Comment]!, error: NSError!) -> ()) {
-        var urlString: String = "http://disqus.com/api/3.0/threads/listPosts.json?api_key=\(publicAPIKey)&forum=\(forum)&thread:ident=\(threadIdent)"
+        var urlString = "http://disqus.com/api/3.0/threads/listPosts.json?api_key=\(publicAPIKey)&forum=\(forum)&thread:ident=\(threadIdent)"
       
         var url = NSURL(string: urlString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)! // URL du JSON
-      
         var request = NSURLRequest(URL: url) // Création de la requête HTTP
         var queue = NSOperationQueue()  // Création de NSOperationQueue à laquelle le bloc du gestionnaire est distribué lorsque la demande complète ou échoué
         
@@ -190,7 +189,9 @@ class DisqusManager {
     }
     
     func threadDetails(ident threadIdent: String, completionHandler: (thread: Thread!, error: NSError!) -> ()) {
-        var url = NSURL(string: "https://disqus.com/api/3.0/threads/details.json?api_key=\(publicAPIKey)&forum=\(forum)&thread:ident=\(threadIdent)")! // URL du JSON
+        var urlString = "https://disqus.com/api/3.0/threads/details.json?api_key=\(publicAPIKey)&forum=\(forum)&thread:ident=\(threadIdent)"
+      
+        var url = NSURL(string: urlString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)! // URL du JSON
         var request = NSURLRequest(URL: url) // Création de la requête HTTP
         var queue = NSOperationQueue()  // Création de NSOperationQueue à laquelle le bloc du gestionnaire est distribué lorsque la demande complète ou échoué
         
